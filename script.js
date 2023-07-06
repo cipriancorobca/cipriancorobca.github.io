28b4f18aa2d926afdd8c1c7afa1515efeffd1597
Ciprian <cipriancorobcacc@pm.me>
2023-07-06 14:04:02 +0200

    sidebar finished

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 function ScrollToProjects()
 {
-    
+    document.getElementById("sidebar-navigation-to-projects").setAttribute("class","sidebar-section-button-selected");
+    document.getElementById("sidebar-navigation-to-top").setAttribute("class","sidebar-section-button");
+    document.getElementById("sidebar-navigation-to-contact").setAttribute("class","sidebar-section-button");
+
+    document.getElementById("sidebar-navigation-to-top").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-contact").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-projects").style.fontWeight = "bolder";
+
+    document.getElementById("sidebar-navigation-title").innerHTML = "Projects";
+}
+
+function ScrollToTop()
+{
+    document.getElementById("sidebar-navigation-to-top").setAttribute("class","sidebar-section-button-selected");
+    document.getElementById("sidebar-navigation-to-projects").setAttribute("class","sidebar-section-button");
+    document.getElementById("sidebar-navigation-to-contact").setAttribute("class","sidebar-section-button");
+
+    document.getElementById("sidebar-navigation-to-projects").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-contact").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-top").style.fontWeight = "bolder";
+
+    document.getElementById("sidebar-navigation-title").innerHTML = "About";
+}
+
+function ScrollToContact()
+{
+    document.getElementById("sidebar-navigation-to-contact").setAttribute("class","sidebar-section-button-selected");
+    document.getElementById("sidebar-navigation-to-top").setAttribute("class","sidebar-section-button");
+    document.getElementById("sidebar-navigation-to-projects").setAttribute("class","sidebar-section-button");
+
+    document.getElementById("sidebar-navigation-to-top").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-projects").style.fontWeight = "normal";
+    document.getElementById("sidebar-navigation-to-contact").style.fontWeight = "bolder";
+
+    document.getElementById("sidebar-navigation-title").innerHTML = "Contact";
 }
