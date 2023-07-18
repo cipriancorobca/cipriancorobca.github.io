03c1b50c50a4a5257c815c230ebe02de48cde463
Ciprian <cipriancorobcacc@pm.me>
2023-07-18 17:04:02 +0200

    cipriancorobca

--- script.js
+++ script.js
@@ -52,23 +52,67 @@ function ToggleAbout(a)
 
     if(a == '1')
     {
-        alert("hi there");
-        ab2.style.opacity = "0";
         ab1.style.transition = "0.6s opacity";
         ab1.style.opacity = "0";
-        setTimeout(function(){ab1.style.display = "none"}, 1000);
+        setTimeout(function(){ab1.style.display = "none"}, 600);
         ab2.style.display = "block";
         ab2.style.transition = "0.6s opacity";
-        ab2.style.opacity = "1";
+        setTimeout(function(){ab2.style.opacity = "1"}, 600);
     }
 
     else if(a == '0')
     {
-        ab2.style.opacity = "0";
         ab2.style.transition = "0.6s opacity";
-        ab2.style.display = "none";
-        ab1.style.opacity = "1";
-        ab1.style.display = "block";
+        ab2.style.opacity = "0";
+        setTimeout(function(){ab2.style.display = "none"}, 600);
+        setTimeout(function(){ab1.style.display = "block"}, 600);
+        ab1.style.opacity = "0";
         ab1.style.transition = "0.6s opacity";
+        ab1.style.opacity = "1";
+    }
+}
+
+function ShowProjectInfo(a)
+{
+    var p1 = document.getElementById("project-info-1");
+    var p2 = document.getElementById("project-info-2");
+    var b1 = document.getElementById("project-1");
+    var b2 = document.getElementById("project-2");
+    var b3 = document.getElementById("project-3");
+
+    if(a == "0")
+    {
+        p1.style.display = "none";
+        p2.style.display = "none";
+        b1.setAttribute("onclick","ShowProjectInfo(1)");
+        b2.setAttribute("onclick","ShowProjectInfo(2)");
+        b3.setAttribute("onclick","ShowProjectInfo(3)");
+    }
+
+    else if(a == "1")
+    {
+        p1.style.display = "block";
+        p2.style.display = "none";
+        b1.setAttribute("onclick","ShowProjectInfo(0)");
+        b2.setAttribute("onclick","ShowProjectInfo(2)");
+        b3.setAttribute("onclick","ShowProjectInfo(3)");
+    }
+
+    else if(a == "2")
+    {
+        p1.style.display = "none";
+        p2.style.display = "block";
+        b1.setAttribute("onclick","ShowProjectInfo(1)");
+        b2.setAttribute("onclick","ShowProjectInfo(0)");
+        b3.setAttribute("onclick","ShowProjectInfo(3)");
+    }
+
+    else if(a == "3")
+    {
+        p1.style.display = "none";
+        p2.style.display = "none";
+        b1.setAttribute("onclick","ShowProjectInfo(1)");
+        b2.setAttribute("onclick","ShowProjectInfo(2)");
+        b3.setAttribute("onclick","ShowProjectInfo(0)");
     }
 }
