ba41bf3ffd99b5a67e7dfd77cfff1bd46e24571e
Ciprian <cipriancorobcacc@pm.me>
2023-08-13 15:48:18 +0300

    c

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function() 
+/*window.onload = function() 
 {
     window.scrollTo(0,0);
     var d,alt = true;
@@ -17,11 +17,11 @@ window.onload = function()
         else
         {
             document.getElementById("sidebar-navigation-title").innerHTML = "" +
-            ("0" + d.getHours()).slice(-2) + "   ​" + ("0" + d.getMinutes()).slice(-2);
+            ("0" + d.getHours()).slice(-2) + " " + ("0" + d.getMinutes()).slice(-2);
             alt = true;
         }
     },1000);
-}
+}*/
 
 function ScrollToProjects()
 {
@@ -69,71 +69,18 @@ function ToggleAbout(a)
 
 function ShowProjectInfo(a)
 {
-    if(window.innerWidth > "930")
+    if(a == "1")
     {
-        var p1 = document.getElementById("project-info-1");
-        var p2 = document.getElementById("project-info-2");
-        var p3 = document.getElementById("project-info-3");
-        var b1 = document.getElementById("project-1");
-        var b2 = document.getElementById("project-2");
-        var b3 = document.getElementById("project-3");
-
-        if(a == "0")
-        {
-            p1.style.display = "none";
-            p2.style.display = "none";
-            p3.style.display = "none";
-            b1.setAttribute("onclick","ShowProjectInfo(1)");
-            b2.setAttribute("onclick","ShowProjectInfo(2)");
-            b3.setAttribute("onclick","ShowProjectInfo(3)");
-        }
-
-        else if(a == "1")
-        {
-            p1.style.display = "block";
-            p2.style.display = "none";
-            p3.style.display = "none";
-            b1.setAttribute("onclick","ShowProjectInfo(0)");
-            b2.setAttribute("onclick","ShowProjectInfo(2)");
-            b3.setAttribute("onclick","ShowProjectInfo(3)");
-        }
-
-        else if(a == "2")
-        {
-            p1.style.display = "none";
-            p2.style.display = "block";
-            p3.style.display = "none";
-            b1.setAttribute("onclick","ShowProjectInfo(1)");
-            b2.setAttribute("onclick","ShowProjectInfo(0)");
-            b3.setAttribute("onclick","ShowProjectInfo(3)");
-        }
-
-        else if(a == "3")
-        {
-            p1.style.display = "none";
-            p2.style.display = "none";
-            p3.style.display = "block";
-            b1.setAttribute("onclick","ShowProjectInfo(1)");
-            b2.setAttribute("onclick","ShowProjectInfo(2)");
-            b3.setAttribute("onclick","ShowProjectInfo(0)");
-        }
+        window.open("https://saudipearl4.wordpress.com","_blank");
     }
 
-    else
+    else if(a == "2")
     {
-        if(a == "1")
-        {
-            window.open("https://github.com/cipriancorobca/giornalino","_blank");
-        }
+        window.open("./untitled_9/source/login/login.html","_blank");
+    }
 
-        else if(a == "2")
-        {
-            window.open("https://github.com/cipriancorobca/untitled_9","_blank");
-        }
-
-        else if(a == "3")
-        {
-            window.open("https://github.com/cipriancorobca/cipriancorobca.github.io","_blank");
-        }
+    else if(a == "3")
+    {
+        window.open("https://github.com/cipriancorobca/cipriancorobca.github.io","_blank");
     }
 }
